445b85750f55eb9adaea648c7938033bc20e2370
reela <sudanrela123@gmail.com>
2024-08-13 15:32:29 +0000

    tst

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+"""definie _hash_password function"""
+import bcrypt
+from uuid import uuid4
+from sqlalchemy.orm.exc import NoResultFound
+from typing import (
+    TypeVar,
+    Union
+)
+
+from db import DB
+from user import User
+
+U = TypeVar(User)
+
+
+def _hash_password(password: str) -> bytes:
+    """hashes a password string and returns it in bytes func"""
+    paswd = password.encode('utf-8')
+    return bcrypt.hashpw(paswd, bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """generate a uuid and return its string representation"""
+    return str(uuid4())
+
+
+class Auth:
+    """auth class to interact with the authentication database"""
+
+    def __init__(self) -> None:
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """register a new user and return a user object"""
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            hashed = _hash_password(password)
+            usr = self._db.add_user(email, hashed)
+            return usr
+        raise ValueError(f"User {email} already exists")
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """validate a user's login credential"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+
+        user_password = user.hashed_password
+        passwd = password.encode("utf-8")
+        return bcrypt.checkpw(passwd, user_password)
+
+    def create_session(self, email: str) -> Union[None, str]:
+        """create a session_id for an existing user and update the user's"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+        return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> Union[None, U]:
+        """takes a session_id and returns the corresponding user"""
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+
+        return user
+
+    def destroy_session(self, user_id: int) -> None:
+        """take a user_id and destroy that user's session"""
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except ValueError:
+            return None
+        return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """generates a reset_token uuid for a user identified by the given email"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """updates a user's password"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError()
+
+        hashed = _hash_password(password)
+        self._db.update_user(user.id, hashed_password=hashed, reset_token=None)
---
+++ 0x03-user_authentication_service/main.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+"""Main file"""
+import requests
+
+
+def register_user(email: str, password: str) -> None:
+    """test for register a user with the given email and password"""
+    resp = requests.post('http://127.0.0.1:5000/users',
+                         data={'email': email, 'password': password})
+    if resp.status_code == 200:
+        assert (resp.json() == {"email": email, "message": "user created"})
+    else:
+        assert(resp.status_code == 400)
+        assert (resp.json() == {"message": "email already registered"})
+
+
+def log_in_wrong_password(email: str, password: str) -> None:
+    """test for log in with the given wrong credentials"""
+    req = requests.post('http://127.0.0.1:5000/sessions',
+                      data={'email': email, 'password': password})
+    assert (req.status_code == 401)
+
+
+def profile_unlogged() -> None:
+    """test for profile without being logged in with sessien_id"""
+    req = requests.get('http://127.0.0.1:5000/profile')
+    assert(req.status_code == 403)
+
+
+def log_in(email: str, password: str) -> str:
+    """test for log in with the given correct email and password"""
+    resp = requests.post('http://127.0.0.1:5000/sessions',
+                         data={'email': email, 'password': password})
+    assert (resp.status_code == 200)
+    assert(resp.json() == {"email": email, "message": "logged in"})
+    return resp.cookies['session_id']
+
+
+def profile_logged(session_id: str) -> None:
+    """test for profile with being logged in with session_id"""
+    cookies = {'session_id': session_id}
+    req = requests.get('http://127.0.0.1:5000/profile',
+                     cookies=cookies)
+    assert(req.status_code == 200)
+
+
+def log_out(session_id: str) -> None:
+    """test for log out with the given session_id"""
+    cookies = {'session_id': session_id}
+    req = requests.delete('http://127.0.0.1:5000/sessions',
+                        cookies=cookies)
+    if req.status_code == 302:
+        assert(req.url == 'http://127.0.0.1:5000/')
+    else:
+        assert(req.status_code == 200)
+
+
+def reset_password_token(email: str) -> str:
+    """test for reset password token with the given email"""
+    req = requests.post('http://127.0.0.1:5000/reset_password',
+                      data={'email': email})
+    if req.status_code == 200:
+        return req.json()['reset_token']
+    assert(req.status_code == 401)
+
+
+def update_password(email: str, reset_token: str,
+                    new_password: str) -> None:
+    """test for update password with the given email"""
+    data = {'email': email, 'reset_token': reset_token,
+            'new_password': new_password}
+    req = requests.put('http://127.0.0.1:5000/reset_password',
+                     data=data)
+    if req.status_code == 200:
+        assert(req.json() == {"email": email, "message": "Password updated"})
+    else:
+        assert(req.status_code == 403)
+
+
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+
+
+if __name__ == "__main__":
+
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
